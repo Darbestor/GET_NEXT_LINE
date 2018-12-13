67ff144cd21d4c6d109298abb69323b297310371
Gulum-ei Halvorson <ghalvors@e2r12p3.21-school.ru>
2018-12-13 17:26:38 +0300

    search_newline error catching

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ghalvors <ghalvors@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 17:36:25 by ghalvors          #+#    #+#             */
-/*   Updated: 2018/12/13 16:44:52 by ghalvors         ###   ########.fr       */
+/*   Updated: 2018/12/13 17:26:13 by ghalvors         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ static char		*ft_str_increase(char *s1, long long len1, long long len2)
 			free(s1);
 		return (NULL);
 	}
-
  	if (!len1)
 		return (conc);
  	ft_memmove(conc, s1, len1);
@@ -72,20 +71,23 @@ static int		ft_search_newline(char *str, char **line, t_gnl *gnl, long long j)
 	while (str && gnl->len < j)
 		if (str[gnl->len++] == '\n')
 		{
-			*line = ft_strnew(gnl->len - 1);
+			if (!(*line = ft_strnew(gnl->len - 1)))
+				return (0);
 			i = 0;
 			while (i++ < gnl->len - 1)
 				(*line)[i - 1] = str[i - 1];
 			gnl->len = j - i;
-			if (gnl->len && (gnl->line = ft_strnew(gnl->len)))
+			if (gnl->len)
 			{
+ 				if (!(gnl->line = ft_strnew(gnl->len)))
+				 	return (0);
 				j = 0;
 				while (j < gnl->len)
 					gnl->line[j++] = str[i++];
 			}
-			return (1);
+			return (2);
 		}
-	return (0);
+	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
@@ -94,28 +96,30 @@ int				get_next_line(const int fd, char **line)
 	t_gnl			*temp;
 	char			*str;
 	long long int	size;
+	int				er;
 
 	if (fd < 0 || !line || !(temp = ft_find_fd(fd, &gnl)))
 		return (-1);
-	if (ft_search_newline(temp->line, line, temp, temp->len))
+	if ((er = ft_search_newline(temp->line, line, temp, temp->len)) == 2)
 		return (1);
 	str = ft_str_increase(temp->line, temp->len, BUFF_SIZE);
-	while ((size = read(temp->fd, str + temp->len, BUFF_SIZE)) > 0 && str)
+	while ((size = read(temp->fd, str + temp->len, BUFF_SIZE)) > 0 && str && er)
 	{
-		if (ft_search_newline(str, line, temp, (temp->len += size)))
+		if ((er = ft_search_newline(str, line, temp, (temp->len += size))) == 2)
 			return (1);
 		str = ft_str_increase(str, temp->len, BUFF_SIZE);
 	}
-	if (size < 0)
-		return (-1);
-	if (temp->len)
+	if (temp->len && size > -1)
 	{
 		*line = ft_strnew(temp->len);
 		while (temp->len--)
 			(*line)[temp->len] = str[temp->len];
 		return (1);
 	}
-	return (0);
+	return (size <= -1 ? -1 : 0);
 }
 
+/* int	ft_memclr(char *str, char **line, t_gnl *temp, int size)
+{
 
+} */
